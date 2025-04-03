32dbd92600bcbbaf284e0544063dd30f93e51a11
otavio lima <otavio.p.lima@aluno.senai.br>
2025-04-03 10:00:57 -0300

    vetor nomes

--- main.go
+++ main.go
@@ -8,4 +8,13 @@ func main() {
 	fmt.Println(numeros) 
 	numeros = append(numeros, 6, 7, 8)
 	fmt.Println(numeros, len(numeros), cap(numeros))
+	// Slice
+	nomes := []string{"otavio", "denise", "fabricio", "brenda", "lucas"}
+	fmt.Println(nomes)
+	rangeOne := nomes[0:2]
+	fmt.Println(rangeOne)
+	rangeTwo := nomes[3:5]
+	fmt.Println(rangeTwo)
+	rangeThree := nomes[2]
+	fmt.Println(rangeThree)
 }
